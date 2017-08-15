4eff46dee5739f885da958b66433a0d5bc9d5867
Mark <mamian521@gmail.com>
2017-08-15 18:35:59 +0800

    change sh to txt

--- leiran_yace.sh
+++ leiran_yace.sh
@@ -2,7 +2,7 @@
 a=$1
 b=${a%.*}
 
-file="yacezlr.sh"
+file="yacezlr.txt"
 for d in `cat $file`
 do
 #echo ${d:0-1}
@@ -12,7 +12,7 @@ sed -i 's/"ThreadGroup.scheduler">.*</"ThreadGroup.scheduler">false</g' $a
 sed -i 's/"ThreadGroup.duration">.*</"ThreadGroup.duration"></g' $a
 sed -i 's/"ThreadGroup.num_threads">.*</"ThreadGroup.num_threads">'${d}'</g' $a
 sh jmeter.sh -n -t $a -l ${b}_${d}_1.jtl
-else 
+else
 d=${d%c}
 sed -i 's/"LoopController.loops">.*</"LoopController.loops">-1</g' $a
 sed -i 's/"ThreadGroup.scheduler">.*</"ThreadGroup.scheduler">true</g' $a
